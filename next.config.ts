577fe8c493ff01e87274048563d6c83e691e4da2
Julian Suringa <juliansuringa@gmail.com>
2025-08-01 23:28:34 +0800

    Initial fix github pages issue not found

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: "export",
+  basePath: "/latest-next-portfolio", // if not deploying to root
+  assetPrefix: "/latest-next-portfolio",
   images: { unoptimized: true },
 };
 
