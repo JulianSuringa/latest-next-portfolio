cf24ce6de42ac668a14b5b0220f9636acc07763e
Julian Suringa <juliansuringa@gmail.com>
2025-08-05 00:17:20 +0800

    Fix footer social icon color in dark mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,55 +1,41 @@
 import Link from "next/link";
 import socials from "@/data/socials.json";
-import Image from "next/image";
-import { prefix } from "@/utils/prefix";
-
-const socialLinks = socials.map(({ name, href }) => (
-  <li key={name}>
-    <a href={href} rel="noreferrer" target="_blank" className="inline-block">
-      {name === "facebook" ? (
-        <Image
-          src={`${prefix}/icons/socials/facebook.svg`}
-          alt="Facebook"
-          width={30}
-          height={30}
-          className="w-7 h-7"
-        />
-      ) : name === "instagram" ? (
-        <Image
-          src={`${prefix}/icons/socials/linkedin.svg`}
-          alt="Instagram"
-          width={30}
-          height={30}
-          className="w-7 h-7"
-        />
-      ) : name === "twitter" ? (
-        <Image
-          src={`${prefix}/icons/socials/twitter.svg`}
-          alt="Twitter"
-          width={30}
-          height={30}
-          className="w-7 h-7"
-        />
-      ) : (
-        <Image
-          src={`${prefix}/icons/socials/insta.svg`}
-          alt="LinkedIn"
-          width={30}
-          height={30}
-          className="w-7 h-7"
-        />
-      )}
-    </a>
-  </li>
-));
+import Facebook from "@/components/icons/facebook";
+import { useTheme } from "next-themes";
+import Instagram from "./icons/instagram";
+import Twitter from "./icons/twitter";
+import Linkedin from "./icons/linkedin";
 
 export default function Footer() {
+  const { theme, resolvedTheme } = useTheme();
+  const isDark = resolvedTheme === "dark" || theme === "dark";
+  const iconColor = isDark ? "#D3D3D3" : "#21243D";
+
   return (
     <footer className="bg-transparent text-secondary-foreground">
       <div className="flex flex-col items-center justify-center p-10 md:p-20 w-full mx-auto">
         <div className="flex flex-col items-center"></div>
         <ul className="flex  flex-row item-center justify center space-x-4">
-          {socialLinks}
+          {socials.map(({ name, href }) => (
+            <li key={name}>
+              <a
+                href={href}
+                rel="noreferrer"
+                target="_blank"
+                className="inline-block"
+              >
+                {name === "facebook" ? (
+                  <Facebook color={iconColor} className="w-7 h-7" />
+                ) : name === "instagram" ? (
+                  <Instagram color={iconColor} className="w-7 h-7" />
+                ) : name === "twitter" ? (
+                  <Twitter color={iconColor} className="w-7 h-7" />
+                ) : (
+                  <Linkedin color={iconColor} className="w-7 h-7" />
+                )}
+              </a>
+            </li>
+          ))}
         </ul>
         <small className="text-color-gray-500 text-sm mt-4">
           Copyright ©2025 All rights reserved
